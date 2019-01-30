fd16b9db9dbd6971995b0846ad288db3c1327cb8
Illyoung Choi <iychoi@email.arizona.edu>
2019-01-30 16:15:28 -0700

    Add script to remove local ignite data

--- bin/clear_database.sh
+++ bin/clear_database.sh
@@ -2,4 +2,5 @@
 CUR_DIR="$(dirname $(realpath $(test -L "${BASH_SOURCE}" && readlink -f "${BASH_SOURCE}" || echo "${BASH_SOURCE}")))"
 source ${CUR_DIR}/bootstrap.sh
 
-rm -rf ${STARGATE_LIBS}/*
+rm -rf ${STARGATE_STORAGE}/*
+rm -rf ${STARGATE_WORK}/*
